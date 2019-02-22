166b160a881e737a700f0b100e03004e50ebed94
Joey Geelen <joeygeelen@outlook.com>
2019-02-22 15:54:59 +0100

    einde les 22-2

--- _FRONT_/src/app/app.component.ts
+++ _FRONT_/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FrontEnd';
+    title = '(misschien) FEESTJE';
+  // setrelay(x){
+     showIt = false;
+  showLine(){
+    this.showIt = !this.showIt;
+  }
 }
